9fa1839e6592f8169b327a4741a14468c73a952e
Tommy Pyatt <tommypyatt@users.noreply.github.com>
2016-10-02 14:56:46 +0100

    Add pause/continue. Custom timer. Appearence changes

--- js/view.js
+++ js/view.js
@@ -6,7 +6,8 @@
             return {
                 minutes: 0,
                 seconds: 0,
-                running: false
+                running: false,
+                customTimer: 5
             };
         },
 
@@ -14,6 +15,19 @@
             this.timer = setInterval(this.update, 500);
             this.chime = new Audio('https://raw.githubusercontent.com/tommypyatt/productivity-timer/gh-pages/sounds/chime.mp3');
             this.favicon = document.getElementById('favicon');
+            document.body.addEventListener('keyup', this.onKeyUp);
+        },
+
+        onKeyUp: function (event) {
+            if (event.code === 'Space') {
+                if (this.state.running) {
+                    this.pause();
+                } else if (!this.state.running && this.state.remaining > 0) {
+                    this.continue();
+                } else {
+                    this.start(25, 0);
+                }
+            }
         },
 
         start: function (minutes, seconds) {
@@ -25,19 +39,47 @@
         },
 
         update: function () {
-            var remaining, minutes, seconds;
+            var minutes, seconds;
             if (!this.state.running) return;
-            remaining = Math.floor((this.state.endTime - Date.now()) / 1000);
-            seconds = remaining % 60;
+            this.setState({
+                remaining: Math.floor((this.state.endTime - Date.now()) / 1000)
+            });
+            seconds = this.state.remaining % 60;
             seconds = seconds < 10 ? '0' + seconds : seconds; // Append '0' if less than 10
-            minutes = Math.floor(remaining / 60);
+            minutes = Math.floor(this.state.remaining / 60);
             this.setState({
                 minutes: minutes,
                 seconds: seconds
             });
-            if (remaining === 0) {
+            if (this.state.remaining === 0) {
                 this.finished();
             }
+        },
+
+        pause: function () {
+            this.setState({
+                running: false,
+                pausedAt: Date.now()
+            });
+        },
+
+        stop: function () {
+            this.setState({
+                running: false,
+                remaining: 0,
+                minutes: 0,
+                seconds: 0
+            });
+        },
+
+        continue: function () {
+            var now = Date.now();
+            var pausedFor = Date.now() - this.state.pausedAt;
+
+            this.setState({
+                running: true,
+                endTime: this.state.endTime + pausedFor
+            });
         },
 
         finished: function () {
@@ -48,16 +90,99 @@
             this.favicon.href = 'favicon-alert.ico';
         },
 
+        changeCustom: function (event) {
+            this.setState({
+                customTimer: event.target.value
+            });
+        },
+
         render: function () {
             return React.createElement(
                 'div',
-                { className: 'counter' },
+                null,
                 React.createElement(
-                    'p',
-                    null,
-                    this.state.minutes,
-                    ':',
-                    this.state.seconds
+                    'div',
+                    { className: 'counter' },
+                    React.createElement(
+                        'p',
+                        null,
+                        this.state.minutes,
+                        ':',
+                        this.state.seconds
+                    )
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'operation' },
+                    React.createElement(
+                        'div',
+                        { className: 'timers', style: { display: !this.state.running && !this.state.remaining ? 'block' : 'none' } },
+                        React.createElement(
+                            'p',
+                            null,
+                            React.createElement(
+                                'a',
+                                { href: '#', onClick: () => {
+                                        this.start(25, 0);
+                                    } },
+                                'Start pomodoro (25 min, default)'
+                            )
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            React.createElement(
+                                'a',
+                                { href: '#', onClick: () => {
+                                        this.start(15, 0);
+                                    } },
+                                'Start 15 min'
+                            )
+                        ),
+                        React.createElement(
+                            'p',
+                            null,
+                            React.createElement(
+                                'a',
+                                { href: '#', onClick: () => {
+                                        this.start(this.state.customTimer, 0);
+                                    } },
+                                'Start custom (',
+                                this.state.customTimer,
+                                ' minute',
+                                this.state.customTimer == 1 ? '' : 's',
+                                ')'
+                            )
+                        ),
+                        React.createElement('input', { type: 'number', defaultValue: this.state.customTimer, onChange: this.changeCustom })
+                    ),
+                    React.createElement(
+                        'p',
+                        { style: { display: this.state.running ? 'block' : 'none' } },
+                        React.createElement(
+                            'a',
+                            { href: '#', onClick: this.pause },
+                            'Pause'
+                        )
+                    ),
+                    React.createElement(
+                        'p',
+                        { style: { display: this.state.running ? 'block' : 'none' } },
+                        React.createElement(
+                            'a',
+                            { href: '#', onClick: this.stop },
+                            'Stop'
+                        )
+                    ),
+                    React.createElement(
+                        'p',
+                        { style: { display: !this.state.running && this.state.remaining > 0 ? 'block' : 'none' } },
+                        React.createElement(
+                            'a',
+                            { href: '#', onClick: this.continue },
+                            'Continue'
+                        )
+                    )
                 )
             );
         }
--- js/view.jsx
+++ js/view.jsx
@@ -4,7 +4,8 @@
             return {
                 minutes: 0,
                 seconds: 0,
-                running: false
+                running: false,
+                customTimer: 5
             };
         },
 
@@ -12,6 +13,19 @@
             this.timer = setInterval(this.update, 500);
             this.chime = new Audio('https://raw.githubusercontent.com/tommypyatt/productivity-timer/gh-pages/sounds/chime.mp3');
             this.favicon = document.getElementById('favicon');
+            document.body.addEventListener('keyup', this.onKeyUp);
+        },
+
+        onKeyUp: function (event) {
+            if (event.code === 'Space') {
+                if (this.state.running) {
+                    this.pause();
+                } else if (!this.state.running && this.state.remaining > 0) {
+                    this.continue();
+                } else {
+                    this.start(25, 0);
+                }
+            }
         },
 
         start: function (minutes, seconds) {
@@ -23,19 +37,47 @@
         },
 
         update: function () {
-            var remaining, minutes, seconds;
+            var minutes, seconds;
             if (!this.state.running) return;
-            remaining = Math.floor( ( this.state.endTime - Date.now() ) / 1000 );
-            seconds = remaining % 60;
+            this.setState({
+                remaining: Math.floor( ( this.state.endTime - Date.now() ) / 1000 )
+            });
+            seconds = this.state.remaining % 60;
             seconds = (seconds < 10) ? '0' + seconds : seconds; // Append '0' if less than 10
-            minutes = Math.floor(remaining / 60);
+            minutes = Math.floor(this.state.remaining / 60);
             this.setState({
                 minutes: minutes,
                 seconds: seconds
             });
-            if (remaining === 0) {
+            if (this.state.remaining === 0) {
                 this.finished();
             }
+        },
+
+        pause: function () {
+            this.setState({
+                running: false,
+                pausedAt: Date.now()
+            });
+        },
+
+        stop: function () {
+            this.setState({
+                running: false,
+                remaining: 0,
+                minutes: 0,
+                seconds: 0
+            });
+        },
+
+        continue: function () {
+            var now = Date.now();
+            var pausedFor = Date.now() - this.state.pausedAt;
+
+            this.setState({
+                running: true,
+                endTime: this.state.endTime + pausedFor
+            });
         },
 
         finished: function () {
@@ -46,9 +88,40 @@
             this.favicon.href = 'favicon-alert.ico';
         },
 
+        changeCustom: function (event) {
+            this.setState({
+                customTimer: event.target.value
+            });
+        },
+
         render: function () {
-            return <div className='counter'>
-                <p>{this.state.minutes}:{this.state.seconds}</p>
+            return <div>
+                <div className='counter'>
+                    <p>{this.state.minutes}:{this.state.seconds}</p>
+                </div>
+                <div className="operation">
+                    <div className="timers" style={{display: (!this.state.running && !this.state.remaining) ? 'block' : 'none'}}>
+                        <p>
+                            <a href="#" onClick={() => {this.start(25, 0)}}>Start pomodoro (25 min, default)</a>
+                        </p>
+                        <p>
+                            <a href="#" onClick={() => {this.start(15, 0)}}>Start 15 min</a>
+                        </p>
+                        <p>
+                            <a href="#" onClick={() => {this.start(this.state.customTimer, 0)}}>Start custom ({this.state.customTimer} minute{(this.state.customTimer == 1) ? '' : 's'})</a>
+                        </p>
+                        <input type="number" defaultValue={this.state.customTimer} onChange={this.changeCustom} />
+                    </div>
+                    <p style={{display: (this.state.running) ? 'block' : 'none'}}>
+                        <a href="#" onClick={this.pause}>Pause</a>
+                    </p>
+                    <p style={{display: (this.state.running) ? 'block' : 'none'}}>
+                        <a href="#" onClick={this.stop}>Stop</a>
+                    </p>
+                    <p style={{display: (!this.state.running && this.state.remaining > 0) ? 'block' : 'none'}}>
+                        <a href="#" onClick={this.continue}>Continue</a>
+                    </p>
+                </div>
             </div>
         }
     });
